adfb84b00fc46bff8ebcb8af23f4d0927a0708ac
David Soares <davidsoares.dev@gmail.com>
2024-09-08 17:22:31 -0300

    feat: Add database integration
    Adds SQLAlchemy for database integration, allowing persistent storage of data. The database URI is configured for a SQLite database, and the `db` object is initialized for interaction.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config("SQLALCHEMY_DATABASE_URI") = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -8,3 +14,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
