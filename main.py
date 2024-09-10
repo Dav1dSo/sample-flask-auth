d58b0299a4181e3a8c308d2422bbb4f170ac153a
David Soares <davidsoares.dev@gmail.com>
2024-09-09 22:50:51 -0300

    feat: Add basic user management API
    This commit introduces a basic user management API, enabling users to create accounts, login, logout, and retrieve user data. It includes:
    
    - `create-user`: Creates a new user with username and password.
    - `login`: Authenticates a user with username and password.
    - `logout`: Logs out the currently authenticated user.
    - `get-all-user`: Retrieves a paginated list of users.
    - `get-user-by-id`: Retrieves a specific user by ID.
    
    This commit lays the foundation for a more robust user management system in the future.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request
 from models.user import User
 from factory import db
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
 import logging
 
 app = Flask(__name__)
@@ -13,7 +19,8 @@ login_manager = LoginManager()
 
 db.init_app(app)
 login_manager.init_app(app)
-login_manager.login_view ='login'
+login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -45,12 +52,12 @@ def login():
                 return {"msg": "Login realizado com sucesso!"}, 200
 
             return {"error": "Credênciais incorretas!"}, 400
-        return {'error': "Username e password devem ser enviádos!"}, 400
-        
+        return {"error": "Username e password devem ser enviádos!"}, 400
 
     except Exception as err:
         logging.error(f"ERROR AO REALIZAR LOGIN: {type(err)} - {err}")
         return {"error": "Ocorreum erro ao relizar login!"}, 500
+
 
 @app.route("/logout", methods=["GET"])
 @login_required
@@ -58,78 +65,96 @@ def logout():
     try:
         if current_user.is_authenticated:
             logout_user()
-            return {'msg': "Logout realizado com sucesso!"}, 200
-        
-        return {'error': "Não foi possível realizar logout!"}, 400
-        
-        
+            return {"msg": "Logout realizado com sucesso!"}, 200
+
+        return {"error": "Não foi possível realizar logout!"}, 400
+
     except Exception as err:
         logging.error(f"ERROR AO REALIZAR LOGOUT: {type(err)} - {err}")
         return {"error": "Ocorreum erro ao relizar logout!"}, 500
 
-@app.route('/create-user', methods=['POST'])
+
+@app.route("/create-user", methods=["POST"])
 def create_user():
-    try:      
-        user_name = request.json.get('user_name')
-        password = request.json.get('password')
-        
-        username_exists = db.session.query(User).filter(User.username == user_name).first()
-        
+    try:
+        user_name = request.json.get("user_name")
+        password = request.json.get("password")
+
+        username_exists = (
+            db.session.query(User).filter(User.username == user_name).first()
+        )
+
         if username_exists is not None:
             return {"error": "User name já em uso!"}, 409
-        
+
         if user_name != None and password != None:
-            new_user = User(
-                username = user_name,
-                password=password
-            )
+            new_user = User(username=user_name, password=password)
             db.session.add(new_user)
             db.session.commit()
-            
-            return {'msg': "Usuário criado com sucesso!"}, 201
-        
-        return {'error': "User name e password devem ser enviados!"}, 400
-        
+
+            return {"msg": "Usuário criado com sucesso!"}, 201
+
+        return {"error": "User name e password devem ser enviados!"}, 400
+
     except Exception as err:
         db.session.rollback()
         logging.error(f"ERROR AO CRIAR USUÁRIO: {type(err)} - {err}")
         return {"error": "Ocorreum erro ao realizar logout!"}, 500
 
-@app.route('/get-all-user', methods=['GET'])
+
+@app.route("/get-all-user", methods=["GET"])
 @login_required
 def get_all_users():
     try:
-        
+
         page = request.args.get("page", 1, type=int)
         per_page = request.args.get("per_page", 10, type=int)
         filter_user_name = request.args.get("username", None, type=str)
         filter_id = request.args.get("filter_id", None, type=str)
-        
+
         query_users = db.session.query(User)
-        
+
         users_query = query_users
-        
+
         if filter_user_name:
-            users_query = users_query.filter(User.username.ilike(f"%{filter_user_name}%"))
-            
+            users_query = users_query.filter(
+                User.username.ilike(f"%{filter_user_name}%")
+            )
+
         if filter_id:
             users_query = users_query.filter(User.id.ilike(f"%{filter_id}%"))
-        
+
         paginate = users_query.paginate(page=page, per_page=per_page, error_out=False)
-        
-        res = [
-            {
-                "id": user.id,
-                "username": user.username
-            } for user in paginate.items
-        ]
-        
+
+        res = [{"id": user.id, "username": user.username} for user in paginate.items]
+
         return res, 200
-        
+
     except Exception as err:
         db.session.rollback()
         logging.error(f"ERROR AO LISTAR USUÁRIOS: {type(err)} - {err}")
         return {"error": "Ocorreum erro ao listar usuários!"}, 500
+
+
+@app.route("/get-user-by-id/<int:user_id>", methods=["GET"])
+@login_required
+def get_user(user_id):
+    try:
+
+        get_user = db.session.query(User).filter(User.id.ilike(f"%{user_id}%")).first()
+
+        if get_user is None:
+            return {'msg': 'Nenhum usuário encontrado!'}, 404
+        
+        res = {"id": get_user.id, "username": get_user.username}
+
+        return res, 200
+
+    except Exception as err:
+        db.session.rollback()
+        logging.error(f"ERROR AO LISTAR USUÁRIOS: {type(err)} - {err}")
+        return {"error": "Ocorreum erro ao listar usuários!"}, 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
