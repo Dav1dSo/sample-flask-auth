28d495ca0633c7b1f5895882bdfc420aae6e2e81
David Soares <davidsoares.dev@gmail.com>
2024-09-09 21:39:36 -0300

    feat: Add basic Flask login functionality
    Adds basic login functionality using Flask-Login and a SQLite database.
    
    - Adds `User` model with `username` and `password` fields.
    - Implements login endpoint with basic validation.
    - Configures Flask-Login and sets up database connection.
    - Includes a `launch.json` file for debugging with VS Code.

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request
+from models.user import User
+from factory import db
+from flask_login import LoginManager, login_user, current_user
+import logging
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'your-secret-key'
-app.config("SQLALCHEMY_DATABASE_URI") = 'sqlite:///database.db'
+app.config["SECRET_KEY"] = "your-secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-db = SQLAlchemy(app)
+login_manager = LoginManager()
 
-@app.route('/', methods=['GET'])
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view ='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route("/", methods=["GET"])
 def home():
-    return f'hellow word!'
+    return f"Hellow word!"
 
-if __name__ == '__main__':
+
+@app.route("/login", methods=["POST"])
+def login():
+    try:
+        data = request.json
+
+        user_name = data.get("user_name")
+        password = data.get("password")
+
+        if user_name and password:
+            user = db.session.query(User).filter(User.username == user_name).first()
+
+            if user is None:
+                return {"error": "Usuário não encontrado!"}, 404
+
+            if user and password == user.password:
+                login_user(user)
+                print(current_user.is_authenticated)
+                return {"msg": "Login realizado com sucesso!"}, 200
+
+            return {"error": "Credênciais incorretas!"}, 400
+        return {'error': "Username e password devem ser enviádos!"}, 400
+        
+
+    except Exception as err:
+        logging.error(f"ERROR AO REALIZAR LOGIN: {type(err)} - {err}")
+        return {"error": "Ocorreum erro ao relizar login!"}, 500
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
---
+++ models/user.py
@@ -0,0 +1,7 @@
+from flask_login import UserMixin
+from factory import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
