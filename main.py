e708c54b8aaf3e60cc3a05dc68042de7a1580f33
David Soares <davidsoares.dev@gmail.com>
2024-09-09 22:44:45 -0300

    feat: Implement user management endpoints
    Adds endpoints for creating new users, retrieving all users, and filtering users by username and ID. Includes pagination for better user experience and error handling for improved robustness.

--- main.py
+++ main.py
@@ -67,6 +67,69 @@ def logout():
         logging.error(f"ERROR AO REALIZAR LOGOUT: {type(err)} - {err}")
         return {"error": "Ocorreum erro ao relizar logout!"}, 500
 
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    try:      
+        user_name = request.json.get('user_name')
+        password = request.json.get('password')
+        
+        username_exists = db.session.query(User).filter(User.username == user_name).first()
+        
+        if username_exists is not None:
+            return {"error": "User name já em uso!"}, 409
+        
+        if user_name != None and password != None:
+            new_user = User(
+                username = user_name,
+                password=password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            
+            return {'msg': "Usuário criado com sucesso!"}, 201
+        
+        return {'error': "User name e password devem ser enviados!"}, 400
+        
+    except Exception as err:
+        db.session.rollback()
+        logging.error(f"ERROR AO CRIAR USUÁRIO: {type(err)} - {err}")
+        return {"error": "Ocorreum erro ao realizar logout!"}, 500
+
+@app.route('/get-all-user', methods=['GET'])
+@login_required
+def get_all_users():
+    try:
+        
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 10, type=int)
+        filter_user_name = request.args.get("username", None, type=str)
+        filter_id = request.args.get("filter_id", None, type=str)
+        
+        query_users = db.session.query(User)
+        
+        users_query = query_users
+        
+        if filter_user_name:
+            users_query = users_query.filter(User.username.ilike(f"%{filter_user_name}%"))
+            
+        if filter_id:
+            users_query = users_query.filter(User.id.ilike(f"%{filter_id}%"))
+        
+        paginate = users_query.paginate(page=page, per_page=per_page, error_out=False)
+        
+        res = [
+            {
+                "id": user.id,
+                "username": user.username
+            } for user in paginate.items
+        ]
+        
+        return res, 200
+        
+    except Exception as err:
+        db.session.rollback()
+        logging.error(f"ERROR AO LISTAR USUÁRIOS: {type(err)} - {err}")
+        return {"error": "Ocorreum erro ao listar usuários!"}, 500
 
 if __name__ == "__main__":
     app.run(debug=True)
